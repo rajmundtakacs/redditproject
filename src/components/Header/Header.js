770dee5eb282975d4b0a904b4e4c09317f9a812b
mapadu <rajmundtakacs@gmail.com>
2024-11-19 15:28:54 +0100

    Adding home route to Reddit logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header = ({ onSearch }) => {
     return (
         <div className={styles.header}>
 
             <div className={styles.title}>
-                <h1>Reddit</h1>
+                <Link to='/' className={styles.logolink} >Reddit</Link>
             </div>
 
             <div className={styles.searchbar}>
